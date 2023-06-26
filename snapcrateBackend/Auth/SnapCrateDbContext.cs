8a5eb8063a43efce8b44fafdf0fc5170203b6416
gokulmaxi <gokulakams@gmail.com>
2023-06-26 16:17:19 +0530

    [Add] auth controller added

---
+++ snapcrateBackend/Auth/SnapCrateDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace snapcrateBackend.Auth
+{
+    public class SnapCrateDbContext : IdentityDbContext<IdentityUser>
+    {
+        public SnapCrateDbContext(DbContextOptions<SnapCrateDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- snapcrateBackend/Program.cs
+++ snapcrateBackend/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using snapcrateBackend.Auth;
+using System.Text;
+
 namespace snapcrateBackend
 {
     public class Program
@@ -5,6 +13,7 @@ namespace snapcrateBackend
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            ConfigurationManager configuration = builder.Configuration;
 
             // Add services to the container.
 
@@ -12,6 +21,32 @@ namespace snapcrateBackend
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<SnapCrateDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnStr")));
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<SnapCrateDbContext>()
+    .AddDefaultTokenProviders();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            // Adding Jwt Bearer
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWT:ValidAudience"],
+                    ValidIssuer = configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+                };
+            });
 
             var app = builder.Build();
 
@@ -23,10 +58,9 @@ namespace snapcrateBackend
             }
 
             app.UseHttpsRedirection();
-
+            // Authentication & Authorization
+            app.UseAuthentication();
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
